2645ff07c35bc002cd6798354fb08fe297fae4f8
Fury <ultimate.gaming.episodes@gmail.com>
2021-11-11 19:39:57 -0500

    Cleaning stuff up

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CSA
- Chicken Smoothie Assistant Extension
+Stuff to do:
+  1. Set default settings
+  2. Check settings before adding elements to the page
+  3. Add listeners for setting changes to add the elements to the page on a setting update
--- content.js
+++ content.js
@@ -1,9 +1,3 @@
-/*
-Stuff to do:
-  1. Set default settings
-  2. Check settings before adding elements to the page
-  3. Add listeners for setting changes to add the elements to the page on a setting update
-*/
 const firstPageRefreshDelay = 5000;
 const showBumpButton = true;
 const showClearButton = true;
