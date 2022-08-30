03331ad3634f3702617ce7c7442120df4513fad3
ElliotCS <i1Fury@users.noreply.github.com>
2022-08-30 01:34:43 -0400

    Bug fixes & settings menu

--- background.js
+++ background.js
@@ -1,23 +1,54 @@
 // background.js
-var postsUrlRegex = /^https?:\/\/(?:[^./?#]+\.)?chickensmoothie\.com\/Forum\/search\.php*/;
+var postsUrlRegex =
+    /^https?:\/\/(?:[^./?#]+\.)?chickensmoothie\.com\/Forum\/search\.php*/;
 
 const defaults = {
-    'firstpage': true,
-    'bumpbutton': true,
-    'clearbutton': true,
-    'bumpmessage': 'Bump'
+    firstpage: true,
+    bumpbutton: true,
+    clearbutton: true,
+    bumpmessage: "Bump",
 };
-// const settings = {};
-//
-// const initSettings = getAllStorageSyncData().then(items => {
-//     Object.assign(settings, items);
-// });
 
+const settings = {};
+
+async function refresh_settings() {
+    const items = await getAllStorageSyncData();
+    console.log(items);
+    for (const key in defaults) {
+        // set the items[key] over the default
+        if (items[key] === undefined) {
+            chrome.storage.sync.set({ [key]: defaults[key] });
+            settings[key] = defaults[key];
+        } else {
+            settings[key] = items[key];
+        }
+        // settings[key] = items[key] || defaults[key];
+    }
+}
+
+function getAllStorageSyncData() {
+    // Immediately return a promise and start asynchronous work
+    return new Promise((resolve, reject) => {
+        // Asynchronously fetch all data from storage.sync.
+        chrome.storage.sync.get(null, (items) => {
+            // Pass any observed errors down the promise chain.
+            if (chrome.runtime.lastError) {
+                return reject(chrome.runtime.lastError);
+            }
+            // Pass the data retrieved from storage down the promise chain.
+            resolve(items);
+        });
+    });
+}
 
 function onPageLoad(tabId) {
-    chrome.tabs.get(tabId, function(tab) {
+    chrome.tabs.get(tabId, function (tab) {
         if (postsUrlRegex.test(tab.url)) {
-            chrome.tabs.sendMessage(tab.id, {text: 'posts'});
+            console.log(settings);
+            chrome.tabs.sendMessage(tab.id, {
+                text: "posts",
+                settings: settings,
+            });
         }
         // } else if (viewTopicUrlRegex.test(tab.url)) {
         //     chrome.tabs.sendMessage(tab.id, {text: 'viewtopic', GBumpPath: GBumpPath, RBumpPath: RBumpPath});
@@ -25,20 +56,49 @@ function onPageLoad(tabId) {
     });
 }
 
-chrome.webNavigation.onCompleted.addListener(function(details) {
-    onPageLoad(details.tabId);
+chrome.webNavigation.onCompleted.addListener(function (details) {
+    refresh_settings()
+        .then(() => {
+            onPageLoad(details.tabId);
+        })
+        .catch(console.error);
 });
 
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message === "get-latest-settings") {
+        refresh_settings()
+            .then(() => {
+                sendResponse(settings);
+            })
+            .catch(console.error);
+    }
+});
 
 chrome.storage.onChanged.addListener(function (changes, namespace) {
-  for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
-    console.log(
-        `Storage key "${key}" in namespace "${namespace}" changed.`,
-        `Old value was "${oldValue}", new value is "${newValue}".`
-    );
-  }
+    for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+        console.log(
+            `Storage key "${key}" in namespace "${namespace}" changed.`,
+            `Old value was "${oldValue}", new value is "${newValue}".`
+        );
+    }
 });
 
+chrome.runtime.onInstalled.addListener((details) => {
+    // if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+    if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+        chrome.tabs.create(
+            {
+                url: "https://www.chickensmoothie.com/Forum/viewtopic.php?f=5&t=4777498",
+            },
+            function (tab) {
+                console.log(
+                    "New tab launched with https://www.chickensmoothie.com/Forum/viewtopic.php?f=5&t=4777498"
+                );
+            }
+        );
+    }
+    // }
+});
 
 // function getAllStorageSyncData() {
 //   return new Promise((resolve, reject) => {
--- popup/popup.js
+++ popup/popup.js
@@ -1,21 +1,76 @@
-const settings = ['firstpage', 'bumpbutton', 'clearbutton'];
+const settings = [
+    {
+        name: "firstpage",
+        type: "toggle",
+    },
+    {
+        name: "bumpbutton",
+        type: "toggle",
+    },
+    {
+        name: "clearbutton",
+        type: "toggle",
+    },
+    {
+        name: "bumpmessage",
+        type: "text",
+    },
+];
+
+let names = settings.map(setting => setting.name);
 
 function handleSwitch(el) {
     let setting = el.id;
     let enabled = el.checked;
-    if (settings.includes(setting)) {
-        let payload = {}
-        payload[setting] = enabled
+    if (names.includes(setting)) {
+        let payload = {};
+        payload[setting] = enabled;
         chrome.storage.sync.set(payload);
     }
 }
 
-settings.forEach(setting => {
-    chrome.storage.sync.get(setting, (data) => {
-        let el = document.getElementById(setting);
-        el.checked = data[setting];
-        el.onclick = function () { handleSwitch(this) }
-        el.parentNode.parentNode.classList.add('show');
+function handleText(e) {
+    let el = e.target;
+    let setting = el.id;
+    let value = el.value;
+    if (names.includes(setting)) {
+        let payload = {};
+        payload[setting] = value;
+        chrome.storage.sync.set(payload);
+    }
+}
+
+settings.forEach((setting) => {
+    chrome.storage.sync.get(setting.name, (data) => {
+        let el = document.getElementById(setting.name);
+        // set el.checked to data[setting] if it exists
+
+        if (setting.type === "toggle") {
+            if (data[setting.name] !== undefined) {
+                el.checked = data[setting.name];
+            } else {
+                el.checked = true;
+            }
+            el.onclick = function () {
+                handleSwitch(this);
+            };
+
+            el.parentNode.parentNode.classList.add("show");
+        }
+        if (setting.type === "text") {
+            el.value = data[setting.name];
+            el.parentNode.classList.add("show");
+            el.addEventListener('input', handleText);
+        }
     });
 });
 
+let textElements = document.getElementsByClassName("text")
+// add the show class to all of them
+for (let i = 0; i < textElements.length; i++) {
+    textElements[i].classList.add("show");
+}
+
+// chrome.storage.sync.get('bumpmessage', (data) => {
+//     console.log(data);
+// });
